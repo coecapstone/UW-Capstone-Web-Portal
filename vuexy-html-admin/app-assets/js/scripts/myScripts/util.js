a55ca0aef86762a7e76e081fae53a9629b7d0315
Charlie Root <root@wax-tablet.wnf.uw.edu>
2021-01-11 17:09:42 -0800

    Create a util.js to handle loading multiple script files into a single HTML file. Move baseURL into util.js as well, rather than defining it in every script file. Correct a syntax error in vendors.min.js.

---
+++ vuexy-html-admin/app-assets/js/scripts/myScripts/util.js
@@ -0,0 +1,24 @@
+// Site-wide configuration
+//const baseURL = "https://uwcoe-api.azurewebsites.net/api/";
+const baseURL = "http://engine.nano.uw.edu/api/";
+
+/*
+ * Simon Willison's addLoadEvent function,
+ * frmo https://www.htmlgoodies.com/beyond/javascript/article.php/3724571/using-multiple-javascript-onload-functions.htm
+ *
+ * usage: addLoadEvent(func);
+ *     or addLoadEvent(function() { some code; });
+ */
+function addLoadEvent(func) {
+  var oldonload = window.onload;
+  if (typeof window.onload != 'function') {
+    window.onload = func;
+  } else {
+    window.onload = function() {
+      if (oldonload) {
+        oldonload();
+      }
+      func();
+    }
+  }
+}
